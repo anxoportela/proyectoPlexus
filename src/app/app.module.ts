9da8dc177a59c90290704823153ec0c6f3b18789
Anxo Portela-Insua Blanco <anxoportela@gmail.com>
2021-02-11 00:57:14 +0100

    update: añadidos imports y providers

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,11 @@ import { AppComponent } from './app.component';
 
 import { FormsModule } from '@angular/forms';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { AngularFireModule } from "@angular/fire";
+import { AngularFireAuthModule } from "@angular/fire/auth";
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { environment } from '../environments/environment';
+import { AuthService } from "./shared/services/auth/auth.service";
 import { FooterComponent } from './components/footer/footer.component';
 import { HeaderComponent } from './components/header/header.component';
 import { BuscadorComponent } from './components/buscador/buscador.component';
@@ -14,6 +19,11 @@ import { OfertasComponent } from './components/ofertas/ofertas.component';
 import { RecomendadosComponent } from './components/recomendados/recomendados.component';
 import { AlojamientosComponent } from './components/alojamientos/alojamientos.component';
 import { InicioComponent } from './components/inicio/inicio.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { SignInComponent } from './components/sign-in/sign-in.component';
+import { SignUpComponent } from './components/sign-up/sign-up.component';
+import { ForgotPasswordComponent } from './components/forgot-password/forgot-password.component';
+import { VerifyEmailComponent } from './components/verify-email/verify-email.component';
 
 @NgModule({
   declarations: [
@@ -25,6 +35,11 @@ import { InicioComponent } from './components/inicio/inicio.component';
     RecomendadosComponent,
     AlojamientosComponent,
     InicioComponent,
+    DashboardComponent,
+    SignInComponent,
+    SignUpComponent,
+    ForgotPasswordComponent,
+    VerifyEmailComponent,
   ],
   imports: [
     BrowserModule,
@@ -32,8 +47,11 @@ import { InicioComponent } from './components/inicio/inicio.component';
     FormsModule,
     FontAwesomeModule,
     NgbModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireAuthModule,
+    AngularFirestoreModule,
   ],
-  providers: [],
+  providers: [AuthService],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
