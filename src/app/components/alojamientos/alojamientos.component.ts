a6cb4dbe1be1bd2d712eda70bd6e1e6230088089
Anxo Portela-Insua Blanco <anxoportela@gmail.com>
2021-02-10 01:04:05 +0100

    update: añadidos datos con json, pruebas con sliders y ngfor

--- src/app/components/alojamientos/alojamientos.component.ts
+++ src/app/components/alojamientos/alojamientos.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import datosEjemplo from '../../../assets/data.json';
 
 @Component({
   selector: 'app-alojamientos',
@@ -7,9 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AlojamientosComponent implements OnInit {
 
+  Datos: any = datosEjemplo;
+
   constructor() { }
 
   ngOnInit(): void {
+
   }
 
 }
--- src/app/components/recomendados/recomendados.component.ts
+++ src/app/components/recomendados/recomendados.component.ts
@@ -1,13 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { NgbCarouselConfig } from '@ng-bootstrap/ng-bootstrap';
+import datosEjemplo from '../../../assets/data.json';
 
 @Component({
   selector: 'app-recomendados',
   templateUrl: './recomendados.component.html',
-  styleUrls: ['./recomendados.component.css']
+  styleUrls: ['./recomendados.component.css'],
+  providers: [NgbCarouselConfig]
 })
 export class RecomendadosComponent implements OnInit {
 
-  constructor() { }
+  Datos: any = datosEjemplo;
+
+  constructor(config: NgbCarouselConfig) {
+    config.interval = 2000;
+    config.keyboard = true;
+    config.pauseOnHover = true;
+  }
 
   ngOnInit(): void {
   }
