f8283a5572eb9b7513c227765e94718a2d4b6c60
Anxo Portela-Insua Blanco <anxoportela@gmail.com>
2021-02-12 02:48:32 +0100

    update: añadida segunda capa de protección

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AuthGuard } from "./shared/guard/auth.guard";
+import { SecureInnerPagesGuard } from "./shared/guard/secure-inner-pages.guard";
 import { SignInComponent } from './components/sign-in/sign-in.component';
 import { SignUpComponent } from './components/sign-up/sign-up.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { ForgotPasswordComponent } from './components/forgot-password/forgot-password.component';
-import { AuthGuard } from "./shared/guard/auth.guard";
 import { VerifyEmailComponent } from './components/verify-email/verify-email.component';
 import { AlojamientosComponent } from './components/alojamientos/alojamientos.component';
 import { InicioComponent } from './components/inicio/inicio.component';
@@ -13,11 +14,11 @@ const routes: Routes = [
   { path: '', redirectTo: '/inicio', pathMatch: 'full' },
   { path: 'inicio', component: InicioComponent },
   { path: 'alojamientos', component: AlojamientosComponent },
-  { path: 'login', component: SignInComponent },
-  { path: 'registro', component: SignUpComponent },
+  { path: 'login', component: SignInComponent, canActivate: [SecureInnerPagesGuard] },
+  { path: 'registro', component: SignUpComponent, canActivate: [SecureInnerPagesGuard]  },
   { path: 'cpanel', component: DashboardComponent, canActivate: [AuthGuard] },
-  { path: 'recuperar', component: ForgotPasswordComponent },
-  { path: 'verificar', component: VerifyEmailComponent }
+  { path: 'recuperar', component: ForgotPasswordComponent, canActivate: [SecureInnerPagesGuard]  },
+  { path: 'verificar', component: VerifyEmailComponent, canActivate: [SecureInnerPagesGuard]  }
 ];
 
 @NgModule({
